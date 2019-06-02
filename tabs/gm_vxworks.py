64e454843b0b98c47331bb2a4cf762a9447dd45b
Robert Radloff <rradloff@jlab.org>
2019-06-02 14:37:57 -0400

    first attempt at kill server command

--- tabs/gm_vxworks.py
+++ tabs/gm_vxworks.py
@@ -9,11 +9,16 @@ import tkinter as tk
 from tkinter import ttk
 import utils as u
 
+ KILL_SERVER_1	=5001
+ KILL_SERVER_2	=5002
+ KILL_SERVER_3	=5003
+ KILL_SERVER_4	=5004
+ KILL_SERVER_5	=5005
 
 class VXWorks(tk.Frame):
   def __init__(self, tab):
     self.vx_frame = tk.Frame(tab, bg=u.green_color)
-    tk.Button(self.vx_frame, text='Kill VXWorks Server, CH', bg=u.green_color, width=30).grid(
+    tk.Button(self.vx_frame, text='Kill VXWorks Server, CH', bg=u.green_color, width=30, command=self.kill_server(u.Crate_CH,KILL_SERVER_1)).grid(
         row=0, column=0, padx=2, pady=2)
     tk.Button(self.vx_frame, text='Kill VXWorks Server, Inj', bg=u.green_color, width=30).grid(
         row=1, column=0, padx=2, pady=2)
@@ -22,3 +27,14 @@ class VXWorks(tk.Frame):
     tk.Button(self.vx_frame, text='Kill VXWorks Server, RtSpec', bg=u.green_color, width=30).grid(
         row=3, column=0, padx=2, pady=2)
     self.vx_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+  def kill_server(self, crate, command):
+    packet = [command, command, 0, 0, 0, "Kill Server", "Y"]
+    err_flag, reply = u.send_command(crate packet)
+    
+    if err_flag == u.SOCK_OK:
+      print("Killed VXWorks Server, ")
+
+    else:
+      print("ERROR, Could not access socket.")
+      return -1
